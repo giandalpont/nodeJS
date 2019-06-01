b8510c9bb4735a0fbd8b654f36b522648bab3d42
Gian Dal Pont <giandalpont@gmail.com>
2019-06-01 00:21:07 -0300

    express em ação

--- server.js
+++ server.js
@@ -1,31 +1,51 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const serv = http.createServer((req,res)=>{
+const port = 3030
+const localhost = ''
 
-    var html = ''
-    
-    if(req.url == '/'){
-        html = `
+app.listen(port,()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
+
+app.get('/',(req,res) =>{
+    res.send(
+        `
         <html>
             <h1>Caiu no /</h1>
         </html>
         `
-    }else if(req.url == '/inicial'){
-        html =  `
-        <html>
-            <h1>Caio no /INICIAL</h1>
-        </html>
-        `
-    }else{
-        html = `
-        <html>
-            <h1>Caio no else</h1>
-        </html>
-        `
-    }
-
-    res.end( html )
-
+    )
 })
 
-serv.listen(3030)
+// const http = require('http')
+
+// const serv = http.createServer((req,res)=>{
+
+//     var html = ''
+    
+//     if(req.url == '/'){
+    //         html = `
+    //         <html>
+    //             <h1>Caiu no /</h1>
+    //         </html>
+//         `
+//     }else if(req.url == '/inicial'){
+//         html =  `
+//         <html>
+//             <h1>Caio no /INICIAL</h1>
+//         </html>
+//         `
+//     }else{
+//         html = `
+//         <html>
+//             <h1>Caio no else</h1>
+//         </html>
+//         `
+//     }
+
+//     res.end( html )
+
+// })
+
+// serv.listen(3030)
