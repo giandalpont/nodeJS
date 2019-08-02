0df5c0f829683ae08f69f97b3baa9a77b387b007
Gian Dal Pont <giandalpont@gmail.com>
2019-08-01 21:29:28 -0300

    Rest - efatorando Sistema e Otimizando o código

--- rest/index.js
+++ rest/index.js
@@ -7,7 +7,7 @@ let app = express();
 app.use(parser.urlencoded({ extended: false }));
 app.use(parser.json());
 
-consign().include('routes').into(app);
+consign().include('routes').include('utils').into(app);
 
 app.listen(3000, '127.0.0.1', ()=>{
   console.log('Servidor rodando!');
