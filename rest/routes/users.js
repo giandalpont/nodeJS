a63b4431724dc072f0398a15ede14eb5ab687f9e
Gian Dal Pont <giandalpont@gmail.com>
2019-08-02 00:42:13 -0300

    Rest - Editando um usário

--- rest/routes/users.js
+++ rest/routes/users.js
@@ -46,6 +46,14 @@ module.exports = app=>{
             }
         })
    })
-
-
+   routeId.put((req,res)=>{
+        db.update({ _id: req.params.id }, req.body, err => {
+            if(err){
+                app.utils.error.send( err, req, res )
+            }else{
+              // res.status(200).json(req.body)
+              res.status(200).json(Object.assign(req.params, req.body))
+            }
+        })
+   })
 };
